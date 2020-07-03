1792d88f518c6f2998f95e5962305921f7e42db4
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-07-03 13:21:07 +0900

    add Logout and logout test

---
+++ tests/Feature/LogoutTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class LogoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // テストユーザー作成
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * ログアウトテスト
+     * @test
+     * @return void
+     */
+    public function shoulld_認証済みユーザーをログアウトさせる()
+    {
+        $response = $this->actingAs($this->user)->json('POST', route('logout'));
+
+        $response->assertStatus(200);
+        $this->assertGuest();
+    }
+}
