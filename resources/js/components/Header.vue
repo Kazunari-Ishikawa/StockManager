6a842ebc3d98f084c74f508887ccd873aa3cda72
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-07-03 16:34:30 +0900

    add node-sass and sass-loader, modify Header.vue

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,40 +1,49 @@
 <template>
   <v-sheet color="#55c500">
     <v-toolbar class="px-sm-6" flat color="#55c500" dark>
-      <v-toolbar-title>
+      <v-toolbar-title class="mr-sm-10 mr-3 stockmanager">
         <RouterLink to="/">StockManager</RouterLink>
       </v-toolbar-title>
-
-      <SearchInput />
+      <v-toolbar-items class="username">UserName</v-toolbar-items>
 
       <v-spacer></v-spacer>
 
-      <v-menu offset-y>
-        <template v-slot:activator="{on, attrs}">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item-group color="primary">
-            <v-list-item>
-              <v-list-item-title>個人設定</v-list-item-title>
-            </v-list-item>
-            <v-list-item @click="doLogout">
-              <v-list-item-title>ログアウト</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-menu>
+      <template>
+        <!-- <v-toolbar-items>
+          <v-btn depressed class="login">Qiitaで登録/ログイン</v-btn>
+        </v-toolbar-items>-->
+
+        <v-menu offset-y>
+          <template v-slot:activator="{on, attrs}">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item-group color="primary">
+              <v-list-item>
+                <v-list-item-title>
+                  <a href="/login/qiita" class="login">Qiitaで登録/ログイン</a>
+                </v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>個人設定</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="doLogout">
+                <v-list-item-title>ログアウト</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
+      </template>
     </v-toolbar>
   </v-sheet>
 </template>
 
 <script>
-import SearchInput from "./SearchInput";
 export default {
   components: {
-    SearchInput
+    // SearchInput
   },
   methods: {
     async doLogout() {
@@ -44,3 +53,18 @@ export default {
   }
 };
 </script>
+
+<style scoped lang="scss">
+.stockmanager {
+  font-size: 28px;
+  > a {
+    color: white;
+  }
+}
+.login {
+  text-transform: none;
+}
+.username {
+  align-items: center;
+}
+</style>
